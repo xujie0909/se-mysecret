1a687fcc48aa6f89c3109eaf8f5b0baaae41d9ba
xujie17 <xujie17@58.com>
2020-12-21 07:29:16 +0800

    xujie

--- src/main/java/com/xujie/mysecret/dao/TraceDao.java
+++ src/main/java/com/xujie/mysecret/dao/TraceDao.java
@@ -4,6 +4,9 @@ import com.xujie.mysecret.entity.mark.Trace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author xujie
+ */
 @Repository
 public interface TraceDao extends JpaRepository<Trace, Long> {
 
--- src/main/java/com/xujie/mysecret/utils/CacheUtil.java
+++
@@ -1,6 +0,0 @@
-package com.xujie.mysecret.utils;
-
-public class CacheUtil {
-
-
-}
